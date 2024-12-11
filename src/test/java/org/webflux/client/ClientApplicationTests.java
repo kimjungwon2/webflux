370672b795679ab699ce0b014a36d1bd79f11940
jungwon <mac2001035@gmail.com>
2024-12-11 19:19:38 +0900

    INIT: 프로젝트 시작

---
+++ src/test/java/org/webflux/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.webflux.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
