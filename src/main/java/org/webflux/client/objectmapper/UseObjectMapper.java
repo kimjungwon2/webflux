afd4d5260eee74a3fe0e1a778a6acca1804bb197
jungwon <mac2001035@gmail.com>
2024-12-13 01:14:58 +0900

    FEAT:Json을 Map으로 바꾸기

--- src/main/java/org/webflux/client/objectmapper/UseObjectMapper.java
+++ src/main/java/org/webflux/client/objectmapper/UseObjectMapper.java
@@ -4,6 +4,8 @@ package org.webflux.client.objectmapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.Map;
+
 public class UseObjectMapper {
     public void jsonToObject(){
         String json = "{\"name\":\"John\", \"age\":30}";
@@ -26,5 +28,19 @@ public class UseObjectMapper {
 
         String json = objectMapper.writeValueAsString(user);
         System.out.println(json);
+    }
+
+    public void JsonToMap(){
+        String json = "{\"name\":\"John\", \"age\":\"30\"}";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, String> map = null;
+        try {
+            map = objectMapper.readValue(json, Map.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Map: " + map);
     }
 }
